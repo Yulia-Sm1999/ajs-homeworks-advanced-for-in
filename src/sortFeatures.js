311145fc71c2206fa48e3e8735ca5916ed4fbbab
Юлия Смирнова <dolphin_99@mail.ru>
2022-12-21 21:09:17 +0300

    Different code of function makeArrayByList

--- src/sortFeatures.js
+++ src/sortFeatures.js
@@ -1,42 +1,26 @@
-const obj = {
-  name: 'мечник', health: 10, level: 2, attack: 80, defence: 40,
-};
-
 const makeArrayByList = (object, features) => {
-  function turnObjToArray(key, fromObj, toArray) {
-    const objToPush = {
-      key,
-      value: fromObj[key],
-    };
-    toArray.push(objToPush);
-  }
-
-  const copiedObject = { ...object };
-  const matchedFeatures = [];
-  for (const elem of features) {
-    for (const feature in copiedObject) {
-      if (feature === elem) {
-        turnObjToArray(feature, obj, matchedFeatures);
-        delete copiedObject[feature];
+  const givenArrayOfKeys = features;
+  const sortedArrayOfKeys = (() => {
+    const restKeys = [];
+    for (const key in object) {
+      if (!(givenArrayOfKeys.includes(key))) {
+        restKeys.push(key);
       }
     }
-  }
+    return restKeys.sort();
+  })();
 
-  const ordered = {};
-  Object.keys(copiedObject).sort().forEach((key) => {
-    ordered[key] = copiedObject[key];
+  const FinalArrayOfKeys = [...givenArrayOfKeys, ...sortedArrayOfKeys];
+  const FinalArrayOfEntries = [];
+  FinalArrayOfKeys.forEach((key) => {
+    const objToPush = {
+      key,
+      value: object[key],
+    };
+    FinalArrayOfEntries.push(objToPush);
   });
 
-  const restFeatures = [];
-  for (const feature in ordered) {
-    if (!(features.includes(feature))) {
-      turnObjToArray(feature, ordered, restFeatures);
-    }
-  }
-
-  const sortedObj = [...matchedFeatures, ...restFeatures];
-  return sortedObj;
+  return FinalArrayOfEntries;
 };
 
-makeArrayByList(obj, ['name', 'level']);
 export default makeArrayByList;
